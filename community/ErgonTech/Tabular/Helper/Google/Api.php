ca9d91d89b6497ce72ea5e4a4582a73ee8885361
Matthew Wells <matthew@ergon.tech>
2016-07-29 13:42:02 -0700

    Google API helper now reads API key from config

--- community/ErgonTech/Tabular/Helper/Google/Api.php
+++ community/ErgonTech/Tabular/Helper/Google/Api.php
@@ -2,6 +2,7 @@
 
 class ErgonTech_Tabular_Helper_Google_Api extends Mage_Core_Helper_Abstract
 {
+    const CONFIG_PATH_API_KEY = 'tabular/google_api/api_key';
 
     /**
      * @var Google_Client
@@ -27,7 +28,9 @@ class ErgonTech_Tabular_Helper_Google_Api extends Mage_Core_Helper_Abstract
     public function getGoogleClient()
     {
         if (is_null($this->client)) {
+            $apiKey = Mage::getStoreConfig(static::CONFIG_PATH_API_KEY);
             $this->client = new Google_Client();
+            $this->client->setDeveloperKey($apiKey);
         }
 
         return $this->client;
--- spec/community/ErgonTech/Tabular/Helper/Google/ApiSpec.php
+++ spec/community/ErgonTech/Tabular/Helper/Google/ApiSpec.php
@@ -2,11 +2,23 @@
 
 namespace spec;
 
+use Mage;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class ErgonTech_Tabular_Helper_Google_ApiSpec extends ObjectBehavior
 {
+    private $app;
+    function let()
+    {
+        $this->app = Mage::app();
+    }
+
+    function letGo()
+    {
+        Mage::reset();
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('ErgonTech_Tabular_Helper_Google_Api');
@@ -21,5 +33,18 @@ class ErgonTech_Tabular_Helper_Google_ApiSpec extends ObjectBehavior
     function it_can_get_a_Google_Client()
     {
         $this->getGoogleClient()->shouldReturnAnInstanceOf(\Google_Client::class);
+    }
+
+    function it_reads_api_key_from_system_config(\Mage_Core_Model_Store $store)
+    {
+        $isInstalled = new \ReflectionProperty($this->app, '_isInstalled');
+        $isInstalled->setAccessible(true);
+        $isInstalled->setValue($this->app, true);
+        $this->app->setCurrentStore($store->getWrappedObject());
+        $store->getConfig(\ErgonTech_Tabular_Helper_Google_Api::CONFIG_PATH_API_KEY)
+            ->willReturn('key')
+            ->shouldBeCalled();
+
+        $this->getGoogleClient();
     }
 }
