cdef9babaa56fc86d7d853552c8082b96d9db295
Matthew Wells <matthew@ergon.tech>
2016-08-17 12:42:56 -0700

    Add row transform callback helper

---
+++ community/ErgonTech/Tabular/Helper/RowTransforms.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ErgonTech\Tabular;
+
+use Mage;
+
+class Helper_RowTransforms extends \Mage_Core_Helper_Abstract
+{
+    /**
+     * Look up in config the row transformation callback configured for this profile
+     *
+     * @param Model_Profile $profile
+     * @return string
+     */
+    public function getRowTransformCallbackForProfile(Model_Profile $profile)
+    {
+        $result = Mage::getConfig()->getNode(sprintf('%s/%s/extra/row_transform_callback/options/%s/callback',
+            Model_Source_Profile_Type::CONFIG_PATH_PROFILE_TYPE,
+            $profile->getProfileType(),
+            $profile->getExtra('row_transform_callback')));
+
+        return (string)$result;
+    }
+}
---
+++ spec/community/ErgonTech/Tabular/Helper/RowTransformsSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\ErgonTech\Tabular;
+
+use ErgonTech\Tabular\Model_Profile;
+use ErgonTech\Tabular\Model_Source_Profile_Type;
+use Mage;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class Helper_RowTransformsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(\ErgonTech\Tabular\Helper_RowTransforms::class);
+    }
+
+    function it_gets_the_currently_configured_row_transform(
+        \Mage_Core_Model_Config $config,
+        Model_Profile $profile
+    )
+    {
+        $profileType = 'asdf';
+        $configRowTransform = 'blah';
+        $rowTransformCallback = 'haha';
+
+        $profile->getProfileType()
+            ->willReturn($profileType)
+            ->shouldBeCalled();
+
+        $profile->getExtra('row_transform_callback')
+            ->willReturn($configRowTransform);
+
+        $refMage = new \ReflectionClass(Mage::class);
+        $refConfig = $refMage->getProperty('_config');
+        $refConfig->setAccessible(true);
+        $refConfig->setValue($config->getWrappedObject());
+
+        $config
+            ->getNode(sprintf('%s/%s/extra/row_transform_callback/options/%s/callback',
+                Model_Source_Profile_Type::CONFIG_PATH_PROFILE_TYPE,
+                $profileType,
+                $configRowTransform))
+            ->willReturn($rowTransformCallback);
+
+        $this->getRowTransformCallbackForProfile($profile)->shouldReturn($rowTransformCallback);
+    }
+}
